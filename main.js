6e9dce17157d0c3b3e57074956091a109895b422
DanielPhilosoph <danielph9@gmail.com>
2021-11-07 19:03:38 +0200

    middleware update

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ const morgan = require("morgan");
 const infoRouter = require("./routers/PersonsInfo");
 const app = express();
 const presonsRouter = require("./routers/presons");
+const morganMiddleWare = require("./middlewares/morganMiddleware");
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 // * Middlwares
+app.use(morganMiddleWare);
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-morgan.token("body", function (req, res) {
-  return JSON.stringify(req.body);
-});
 
 // * Routers
 app.use("/api/persons", presonsRouter);
